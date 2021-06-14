6af46037df4745987b503a6d39f5d83c1d4acbde
cj <charles.jobin@gmail.com>
2021-06-13 23:34:37 -0400

    First commit

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM python:3.9.5
+
+RUN apt-get update && apt-get -y install cron
+
+RUN touch /etc/cron.d/website-monitoring-cron /var/log/cron.log && \
+    chmod 0644 /etc/cron.d/website-monitoring-cron && \
+    echo "* 4 * * * python3 /usr/local/bin/send_alert.py >> /var/log/cron.log 2>&1" > /etc/cron.d/website-monitoring-cron && \
+    echo "" >> /etc/cron.d/website-monitoring-cron && \
+    crontab /etc/cron.d/website-monitoring-cron
+
+
+WORKDIR /usr/local/bin
+
+COPY ./send_alert.py .
+
+CMD ["send_alert.py"]
---
+++ send_alert.py
@@ -0,0 +1,40 @@
+import smtplib
+import requests
+import os
+from email.message import EmailMessage
+
+urls = os.getenv('URLS').split(',')
+if not urls:
+    print("No urls provided. Exiting.", file=sys.stderr) 
+    return 1
+
+emailAddress = os.getenv('EMAIL_ADDRESS')
+password = os.getenv('EMAIL_PASSWORD')
+if not emailAddress or not password:
+    print("Email address and password must be defined. Exiting.", file=sys.stderr) 
+    return 1
+#password = "csfgymkwhhpwumpz"
+
+msg = EmailMessage()
+msg['From'] = emailAddress
+msg['To'] = emailAddress
+
+
+# Function to send the message via our own SMTP server.
+def sendAlert(message):
+    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    server.login(emailAddress, password)
+    server.send_message(message)
+    server.quit()
+
+for url in urls:
+    r = requests.get(url)
+    if (r.status_code != 200):
+
+        msg.set_content('Your webserver: ' + url + ' might be down.')
+        msg['Subject'] = '[web-monitoring]: ' + url'
+
+        if __name__ == '__main__':
+            sendAlert(msg)
+
+
