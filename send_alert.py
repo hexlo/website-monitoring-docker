1f14442692288c1364a7268bd36f2eb49ebb7cfd
cj <charles.jobin@gmail.com>
2021-06-14 01:17:04 -0400

    works with valid urls. Still need to catch http errors.

--- send_alert.py
+++ send_alert.py
@@ -8,35 +8,42 @@ from email.message import EmailMessage
 def main():
 
     urls = os.getenv('URLS').split(',')
-    if not urls:
-        print("No urls provided. Exiting.", file=sys.stderr) 
-        return 1
 
     emailAddress = os.getenv('EMAIL_ADDRESS')
     password = os.getenv('EMAIL_PASSWORD')
-    if not emailAddress or not password:
-        print("Email address and password must be defined. Exiting.", file=sys.stderr) 
+    if not emailAddress or emailAddress == "" or not password or password == "":
+        print("Email address and password must be defined. Exiting.")
         return 1
 
-    msg = EmailMessage()
-    msg['From'] = emailAddress
-    msg['To'] = emailAddress
+
 
 
     # Function to send the message via our own SMTP server.
     def sendAlert(message):
+        print("entered sendAlert()")
         server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
         server.login(emailAddress, password)
         server.send_message(message)
         server.quit()
 
     for url in urls:
-        r = requests.get(url)
-        if (r.status_code != 200):
-            msg.set_content('Your webserver: ' + url + ' might be down.')
-            msg['Subject'] = '[web-monitoring]: ' + url
-
-            if __name__ == '__main__':
+        print("entering for loop: " + url)
+        try:
+            r = requests.get(url)
+            print(r)
+            print(r.status_code)
+            if (r.status_code != 200):
+                print("sending email.")
+                msg = EmailMessage()
+                msg['From'] = emailAddress
+                msg['To'] = emailAddress
+                msg.set_content('Your webserver: ' + url + ' might be down. \n Received error code: ' + str(r.status_code))
+                msg['Subject'] = '[web-monitoring]: ' + url
                 sendAlert(msg)
+        except:
+            r.status_code = "Connection Refused"
+            print("Bad URL. Cannot proceed.")
 
 
+if __name__ == '__main__':
+    main()
